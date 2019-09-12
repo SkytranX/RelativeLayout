99a272c4444a03e82334d71244060d6a5867c57a
Steven Tran <StevenSkytran@gmail.com>
2019-09-12 14:07:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/relativelayout/RelativeEmail.java
@@ -0,0 +1,14 @@
+package com.example.relativelayout;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RelativeEmail extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compose_screen);
+    }
+}
